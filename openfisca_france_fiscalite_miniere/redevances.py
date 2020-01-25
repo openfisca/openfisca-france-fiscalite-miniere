9608591cc775632772d12fadefc75c8482979918
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-01-25 16:52:29 +0100

    [WIP] Redevance départementale des mines - Or

--- openfisca_france_fiscalite_miniere/redevances.py
+++ openfisca_france_fiscalite_miniere/redevances.py
@@ -1,3 +1,5 @@
+import numpy
+
 from openfisca_core import indexed_enums
 from openfisca_core import periods
 from openfisca_core import variables
@@ -25,3 +27,19 @@ class quantite(variables.Variable):
     label = "Kilogrammes extraits, dont l'imposition est prévue par la loi"
     reference = "https://bofip.impots.gouv.fr/bofip/264-PGP"
     definition_period = periods.YEAR
+
+
+class redevance_departamentale_des_mines(variables.Variable):
+    value_type = float
+    entity = entities.societe
+    label = "Redevance départementale des mines"
+    reference = "https://www.legifrance.gouv.fr/affichCode.do;jsessionid=A704355998953570ACB83E8718235B23.tplgfr35s_3?idSectionTA=LEGISCTA000006162672&cidTexte=LEGITEXT000006069577&dateTexte=20200122"  # noqa: E501
+    definition_period = periods.YEAR
+
+    def formula(societes, period, parameters) -> numpy.ndarray:
+        annee_imposable = period.last_year()
+        quantites = societes("quantite", annee_imposable)
+        tarifs = parameters(annee_imposable).redevances.departamentale
+        tarif = getattr(tarifs, societes("nature", annee_imposable))
+
+        return quantites * tarif
--- openfisca_france_fiscalite_miniere/tests/test_entities.py
+++ openfisca_france_fiscalite_miniere/tests/test_entities.py
@@ -2,8 +2,6 @@ from openfisca_france_fiscalite_miniere import entities
 
 
 def test_societe():
-    key = "société"
-
     result = entities.societe
 
-    assert result.key == key
+    assert(result)
