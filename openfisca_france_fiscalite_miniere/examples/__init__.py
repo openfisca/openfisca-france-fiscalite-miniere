952972630dc48a2dd6c864c74a9a1d2ee44e685f
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-01-26 18:06:08 +0100

    Ajout de la config pour l'API

---
+++ openfisca_france_fiscalite_miniere/examples/__init__.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def parse(file_name):
+    file_path = os.path.join(DIR_PATH, file_name)
+
+    with open(file_path, "r") as file:
+        return json.loads(file.read())
+
+
+societe = parse("societe.json")
