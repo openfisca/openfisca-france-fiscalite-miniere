7928b70ee1d06b68f57c3ee02e14125f944458e8
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-01-26 18:06:08 +0100

    Ajout de la config pour l'API

--- openfisca_france_fiscalite_miniere/__init__.py
+++ openfisca_france_fiscalite_miniere/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
-from openfisca_france_fiscalite_miniere import entities
+from openfisca_france_fiscalite_miniere import entities, examples
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,3 +12,8 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
         super(CountryTaxBenefitSystem, self).__init__(entities.entities)
         self.add_variables_from_directory(os.path.join(COUNTRY_DIR, 'variables'))
         self.load_parameters(os.path.join(COUNTRY_DIR, 'parameters'))
+        self.open_api_config = {
+            "variable_example": "redevance_communale_des_mines",
+            "parameter_example": "redevances.communale.or",
+            "simulation_example": examples.societe,
+            }
