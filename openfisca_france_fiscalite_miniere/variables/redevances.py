e02a9c983a4a6e57a48add7ddc8bef39eece53a9
sandcha <sandcha@users.noreply.github.com>
2020-02-21 19:01:29 +0100

    Ajoute les variables de redevance de sel raffiné

--- openfisca_france_fiscalite_miniere/variables/redevances.py
+++ openfisca_france_fiscalite_miniere/variables/redevances.py
@@ -114,6 +114,52 @@ class redevance_totale_des_mines_sel_abattage_kt(Variable):
         return departementale + communale
 
 
+class redevance_communale_des_mines_sel_raffine_kt(Variable):
+    value_type = float
+    entity = entities.societe
+    label = "Redevance communale du sel raffiné"
+    # reference ?
+    definition_period = YEAR
+
+    def formula(societes, period, parameters) -> numpy.ndarray:
+        annee_production = period.last_year
+        taux = parameters(period).redevances.communales.sel_raffine
+        quantites = societes("quantite_sel_raffine_kt", annee_production)
+
+        return numpy.round(quantites * taux, decimals = 2)
+
+
+class redevance_departementale_des_mines_sel_raffine_kt(Variable):
+    value_type = float
+    entity = entities.societe
+    label = "Redevance départementale du sel raffiné"
+    # reference ?
+    definition_period = YEAR
+
+    def formula(societes, period, parameters) -> numpy.ndarray:
+        annee_production = period.last_year
+        taux = parameters(period).redevances.departementales.sel_raffine
+        quantites = societes("quantite_sel_raffine_kt", annee_production)
+
+        return numpy.round(quantites * taux, decimals = 2)
+
+
+class redevance_totale_des_mines_sel_raffine_kt(Variable):
+    value_type = float
+    entity = entities.societe
+    label = "Redevance départamentale + communale des mines de sel raffiné (par millier de tonnes)"  # noqa: E501
+    definition_period = YEAR
+
+    def formula(societes, period) -> numpy.ndarray:
+        departementale = societes(
+            "redevance_departementale_des_mines_sel_raffine_kt",
+            period)
+        communale = societes(
+            "redevance_communale_des_mines_sel_raffine_kt",
+            period)
+        return departementale + communale
+
+
 class redevance_totale_des_mines(Variable):
     value_type = float
     entity = entities.societe
