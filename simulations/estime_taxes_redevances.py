5c91c5fa0937bc46622a2f8bd973a787a05fe01d
sandcha <sandcha@users.noreply.github.com>
2020-11-02 20:26:55 +0100

    Initialise la jonction entre titres miniers et communes

--- simulations/estime_taxes_redevances.py
+++ simulations/estime_taxes_redevances.py
@@ -6,15 +6,21 @@ from openfisca_france_fiscalite_miniere import CountryTaxBenefitSystem as France
 
 # INPUT DATA
 
-data_csv_path = "TODO - path vers csv"
 data_period = 2019
+csv_titres = "TODO get csv from camino"  # Camino, export Activités
+csv_communes = "TODO get csv from camino"  # Camino, export Titres miniers et autorisations
 
-production_par_titre = pandas.read_csv(data_csv_path)
+production_par_titre = pandas.read_csv(csv_titres)
+communes_par_titre = pandas.read_csv(csv_communes)
+
 filtre_annee_production = production_par_titre['annee'] == data_period
+titres_data = production_par_titre[filtre_annee_production]
+titres_ids = titres_data.titre_id
 
-simulation_data = production_par_titre[filtre_annee_production]
-print(simulation_data)
-titres_ids = simulation_data.titre_id
+# 'titre_id' des exports d'activités (csv_titres) = 'id' des exports de titres (csv_communes)
+filtre_titres = communes_par_titre['id'].isin(titres_ids.tolist())
+communes_data = communes_par_titre[filtre_titres]
+communes_ids = communes_data.communes
 
 # SIMULATION
 
@@ -27,10 +33,15 @@ simulation_builder = SimulationBuilder()
 simulation_builder.create_entities(tax_benefit_system)
 simulation_builder.declare_person_entity('societe', titres_ids)
 
+# associer communes et titres
+commune_instance = simulation_builder.declare_entity('commune', communes_ids)
+titres_des_communes = communes_par_titre.id # un id par titre existant
+simulation_builder.join_with_persons(commune_instance, titres_des_communes, None)
+
 simulation = simulation_builder.build(tax_benefit_system)
 
 for k, v in production_par_titre_keys.items():
-    simulation.set_input(k, data_period, simulation_data[v])
+    simulation.set_input(k, data_period, titres_data[v])
 
 taxe_guyane_brute = simulation.calculate('taxe_guyane_brute', simulation_period)
 
@@ -41,8 +52,7 @@ estimations = titres_ids
 
 resultat = pandas.DataFrame(estimations, columns = colonnes)
 
-resultat['renseignements_orBrut'] = simulation_data.renseignements_orBrut
+resultat['renseignements_orBrut'] = titres_data.renseignements_orBrut
 resultat['taxe_guyane_brute'] = taxe_guyane_brute
-
 
 resultat.to_csv('estimation.csv', index=False)
