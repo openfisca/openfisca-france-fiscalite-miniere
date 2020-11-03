6b2ffefc764c651ebbd77120d8f5ab6ebd5a5dd4
sandcha <sandcha@users.noreply.github.com>
2020-11-03 14:41:14 +0100

    Ajoute des helpers et date les résultats

--- simulations/estime_taxes_redevances.py
+++ simulations/estime_taxes_redevances.py
@@ -1,4 +1,5 @@
 import pandas  # noqa: I201
+import time
 
 from openfisca_core.simulation_builder import SimulationBuilder  # noqa: I100
 from openfisca_france_fiscalite_miniere import CountryTaxBenefitSystem as FranceFiscaliteMiniereTaxBenefitSystem  # noqa: E501
@@ -29,25 +30,33 @@ communes_ids = communes_data.communes
 
 # SIMULATION
 
-production_par_titre_keys = {'quantite_aurifere_kg': 'renseignements_orNet'}
+def build_simulation(period, titres_ids, communes_ids):
+  tax_benefit_system = FranceFiscaliteMiniereTaxBenefitSystem()
+
+  simulation_builder = SimulationBuilder()
+  simulation_builder.create_entities(tax_benefit_system)
+  simulation_builder.declare_person_entity('societe', titres_ids)
+
+  # associer communes et titres
+  commune_instance = simulation_builder.declare_entity('commune', communes_ids)
+  titres_des_communes = communes_ids  # un id par titre existant
+  titres_communes_roles = ['societe'] * len(titres_des_communes)  # role de chaque titre dans la commune = societe
+  simulation_builder.join_with_persons(commune_instance, titres_des_communes, roles = titres_communes_roles)
+
+  return simulation_builder.build(tax_benefit_system)
+
+
+def set_simulation_inputs(simulation, openfisca_to_data_keys):
+  for k, v in production_par_titre_keys.items():
+    simulation.set_input(k, data_period, titres_data[v])
+  return simulation
+
 
 simulation_period = '2020'
-tax_benefit_system = FranceFiscaliteMiniereTaxBenefitSystem()
+simulation = build_simulation(simulation_period, titres_ids, communes_ids)
 
-simulation_builder = SimulationBuilder()
-simulation_builder.create_entities(tax_benefit_system)
-simulation_builder.declare_person_entity('societe', titres_ids)
-
-# associer communes et titres
-commune_instance = simulation_builder.declare_entity('commune', communes_ids)
-titres_des_communes = communes_ids  # un id par titre existant
-titres_communes_roles = ['societe'] * len(titres_des_communes)  # role de chaque titre dans la commune = societe
-simulation_builder.join_with_persons(commune_instance, titres_des_communes, roles = titres_communes_roles)
-
-simulation = simulation_builder.build(tax_benefit_system)
-
-for k, v in production_par_titre_keys.items():
-    simulation.set_input(k, data_period, titres_data[v])
+production_par_titre_keys = {'quantite_aurifere_kg': 'renseignements_orNet'}
+simulation = set_simulation_inputs(simulation, production_par_titre_keys)
 
 taxe_guyane_brute = simulation.calculate('taxe_guyane_brute', simulation_period)
 
@@ -61,4 +70,5 @@ resultat = pandas.DataFrame(estimations, columns = colonnes)
 resultat['renseignements_orNet'] = titres_data.renseignements_orNet
 resultat['taxe_guyane_brute'] = taxe_guyane_brute
 
-resultat.to_csv('estimation.csv', index=False)
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+resultat.to_csv(f'estimation_{timestamp}.csv', index=False)
