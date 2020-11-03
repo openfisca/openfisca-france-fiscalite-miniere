811ab8d87e1e45d8f050c377e7005a087ace1724
sandcha <sandcha@users.noreply.github.com>
2020-11-03 14:27:43 +0100

    Calcule la taxe_guyane_brute

--- simulations/estime_taxes_redevances.py
+++ simulations/estime_taxes_redevances.py
@@ -4,27 +4,32 @@ from openfisca_core.simulation_builder import SimulationBuilder  # noqa: I100
 from openfisca_france_fiscalite_miniere import CountryTaxBenefitSystem as FranceFiscaliteMiniereTaxBenefitSystem  # noqa: E501
 
 
-# INPUT DATA
+# CONFIGURATION
 
 data_period = 2019
-csv_titres = "TODO get csv from camino"  # Camino, export Activités
-csv_communes = "TODO get csv from camino"  # Camino, export Titres miniers et autorisations
+# Camino, export Titres miniers et autorisations :
+csv_communes = "/Volumes/Transcend2/beta/camino_2020/20201103-12h35-camino-titres-66.csv"
+# Camino, export Activités
+csv_titres = "/Volumes/Transcend2/beta/camino_2020/20201103-12h42-camino-activites-53.csv"
 
-production_par_titre = pandas.read_csv(csv_titres)
-communes_par_titre = pandas.read_csv(csv_communes)
+# ADAPT INPUT DATA
+
+communes_par_titre : pandas.DataFrame = pandas.read_csv(csv_communes)
+production_par_titre : pandas.DataFrame = pandas.read_csv(csv_titres)
 
 filtre_annee_production = production_par_titre['annee'] == data_period
 titres_data = production_par_titre[filtre_annee_production]
 titres_ids = titres_data.titre_id
 
+# selection des communes des titres pour lesquels nous avons des activités
 # 'titre_id' des exports d'activités (csv_titres) = 'id' des exports de titres (csv_communes)
 filtre_titres = communes_par_titre['id'].isin(titres_ids.tolist())
-communes_data = communes_par_titre[filtre_titres]
+communes_data = communes_par_titre[filtre_titres][['id', 'substances', 'communes', 'departements']]
 communes_ids = communes_data.communes
 
 # SIMULATION
 
-production_par_titre_keys = {'quantite_aurifere_kg': 'renseignements_orBrut'}  # NaN ?
+production_par_titre_keys = {'quantite_aurifere_kg': 'renseignements_orNet'}
 
 simulation_period = '2020'
 tax_benefit_system = FranceFiscaliteMiniereTaxBenefitSystem()
@@ -35,8 +40,9 @@ simulation_builder.declare_person_entity('societe', titres_ids)
 
 # associer communes et titres
 commune_instance = simulation_builder.declare_entity('commune', communes_ids)
-titres_des_communes = communes_par_titre.id # un id par titre existant
-simulation_builder.join_with_persons(commune_instance, titres_des_communes, None)
+titres_des_communes = communes_ids  # un id par titre existant
+titres_communes_roles = ['societe'] * len(titres_des_communes)  # role de chaque titre dans la commune = societe
+simulation_builder.join_with_persons(commune_instance, titres_des_communes, roles = titres_communes_roles)
 
 simulation = simulation_builder.build(tax_benefit_system)
 
@@ -47,12 +53,12 @@ taxe_guyane_brute = simulation.calculate('taxe_guyane_brute', simulation_period)
 
 # SIMULATION OUTPUT
 
-colonnes = ['titre_id', 'renseignements_orBrut', 'taxe_guyane_brute']
+colonnes = ['titre_id', 'renseignements_orNet', 'taxe_guyane_brute']
 estimations = titres_ids
 
 resultat = pandas.DataFrame(estimations, columns = colonnes)
 
-resultat['renseignements_orBrut'] = titres_data.renseignements_orBrut
+resultat['renseignements_orNet'] = titres_data.renseignements_orNet
 resultat['taxe_guyane_brute'] = taxe_guyane_brute
 
 resultat.to_csv('estimation.csv', index=False)
