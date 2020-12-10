7b00eceb76143cbb9eefc16d20cd39c9c27c2f0f
sandcha <sandcha@users.noreply.github.com>
2020-12-10 22:24:42 +0100

    Adapte les données aux colonnes officielles de la matrice drfip

--- simulations/drfip.py
+++ simulations/drfip.py
@@ -1,63 +1,120 @@
-# Template MATRICE 1121
-# REDEVANCE DÉPARTEMENTALE ET COMMUNALE DES MINES 
-# (articles 1519 et 1587 à 1589 du Code général des impôts)
-# TAXE MINIÈRE SUR L'OR DE GUYANE
-# (article 1599 quinquies B du Code général des impôts)
+import pandas  # noqa: I201
+import numpy
 
-colonnes_1121 = [
-    "Numéro d'ordre de la matrice",  # [col. 1]
-    "Commune du lieu principal d'exploitation",
-    # [col. 2]
-    # > Camino : commune de plus grande surface dans titres.communes
-    # > au format commune_1 (float surface);commune_2 (float surface)
-    "Désignation et adresse des concessionnaires, titulaires de permis d’exploitation ou exploitants",
-        # [col. 3]
-        # (lorsqu’il s’agit d’une mine amodiée, porter à l’encre rouge la désignation
-        # et l’adresse de l’exploitant au-dessous de celle du concessionnaire)
-        # > Camino : adresse de l'amodiataire ou, à défaut, le titulaire.
-        # > titres.amodiataires_noms/titres.amodiataires_adresses 
-        # > ou titres.titulaires_noms/titres.titulaires_adresses
-        # > désignation en amodiataire/titulaire à mémoriser pour le type d'entreprise
-    "Nature des substances extraites",
-    # [col. 4]
-    # > Camino : titres.domaine
-    # Base des redevances :
-    "Nature",  # [col. 5]
-    # > Camino : titres.substances (séparateur ';')
-    "Quantités",
-    # [col. 6]
-    # > Camino : OR activités.renseignements_orBrut (cellule potentiellement vide)
-    # > renseignements_orExtrait à zéro ; ou renseignements_orNet (si brut non défini ?)
-    # > pour activités.annee en N-1 
-    # > et activités.period se terminant par 'trimestre' (1er trimestre, 2e trimestre, 3e trimestre, 4e trimestre)
+def generate_matrice_drfip_guyane(numeros_ordre, data, annee_production, timestamp):
+    '''
+    Template MATRICE 1121
+    REDEVANCE DÉPARTEMENTALE ET COMMUNALE DES MINES 
+    (articles 1519 et 1587 à 1589 du Code général des impôts)
+    TAXE MINIÈRE SUR L'OR DE GUYANE
+    (article 1599 quinquies B du Code général des impôts)
+    '''
+    colonnes_1121 = [
+        "Numéro d'ordre de la matrice",  # [col. 1]
+        "Commune du lieu principal d'exploitation",
+        # [col. 2]
+        # > Camino : commune de plus grande surface dans titres.communes
+        # > au format commune_1 (float surface);commune_2 (float surface)
+        "Désignation et adresse des concessionnaires, titulaires de permis d’exploitation ou exploitants",
+            # [col. 3]
+            # (lorsqu’il s’agit d’une mine amodiée, porter à l’encre rouge la désignation
+            # et l’adresse de l’exploitant au-dessous de celle du concessionnaire)
+            # > Camino : adresse de l'amodiataire ou, à défaut, le titulaire.
+            # > titres.amodiataires_noms/titres.amodiataires_adresses 
+            # > ou titres.titulaires_noms/titres.titulaires_adresses
+            # > désignation en amodiataire/titulaire à mémoriser pour le type d'entreprise
+        "Nature des substances extraites",
+        # [col. 4]
+        # > Camino : titres.domaine
+        # Base des redevances :
+        "Nature",  # [col. 5]
+        # > Camino : titres.substances (séparateur ';')
+        "Quantités (kg)",
+        # [col. 6]
+        # > Camino : OR activités.renseignements_orBrut (cellule potentiellement vide)
+        # > renseignements_orExtrait à zéro ; ou renseignements_orNet (si brut non défini ?)
+        # > pour activités.annee en N-1 
+        # > et activités.period se terminant par 'trimestre' (1er trimestre, 2e trimestre, 3e trimestre, 4e trimestre)
+        # Redevance départementale :
+        "Tarifs (RDM)",  # [col. 7]
+        "Montant net (RDM)",  # [col. 8] (col. 6 x col. 7)
+        # Redevance communale :
+        "Tarifs (RCM)",  # [col. 9]
+        "Montant net redevance des mines (RCM)",  # [col. 10] (col. 6 x col. 9)
+        # ---
+        "Total redevance des mines",  # [col. 11] (col.8 + col. 10)
+        # Taxe minière sur l'or de Guyane :
+        "PME",  # [col. 12] Tarifs par kg extrait pour les...
+        "Autres entreprises",  # [col. 13] Tarifs par kg extrait pour les...
+        # > Camino : titres.amodiataires_categorie ou titres.titulaires_categorie
+        "Montant des investissements déduits",
+        # [col. 14]
+        # > Camino : somme des 4 rapports trimestriels année N-1 (activités.renseignements_environnement)
+        # > pour activités.annee en N-1 et activités.period en 'année'
+        # > = sur l'interface web, activités, "Dépenses relatives à la protection de l’environnement (euros)"
+        "Montant net de taxe minière sur l'or de Guyane", # [col. 15] (col.6 x col.12)-col.14 ou (col.6 x col.13)-col.14
+        # ---
+        "Frais de gestion de la fiscalité directe locale",  # [col. 16] (col.11 + col.15) x 8%
+        "Service de la Direction générale des Finances publiques en charge du recouvrement",
+        # [col. 17]
+        # > Camino : titre.administrations_noms ?
+        "Numéro de l’article du rôle",  # [col. 18]
+        "Observations, précisions"
+        # [col. 19]
+        # > Camino : activités.complement_texte ?
+    ]  # une ligne par titre
+
+    matrice = pandas.DataFrame(data, columns = colonnes_1121)
+
+    matrice["Numéro d'ordre de la matrice"] = numeros_ordre
+    matrice["Commune du lieu principal d'exploitation"] = data["commune_exploitation_principale"]
+    new_lines = numpy.full(len(numeros_ordre), '\n')
+    prefix_siren = numpy.full(len(numeros_ordre), 'SIREN ')
+    matrice["Désignation et adresse des concessionnaires, titulaires de permis d’exploitation ou exploitants"] = (
+        data["nom_entreprise"] + new_lines
+        + data["adresse_entreprise"] + new_lines + prefix_siren
+        + data["siren"])
+    matrice["Nature des substances extraites"] = data["substances"]
+    matrice["Nature"] = data["substances"]
+    matrice["Quantités (kg)"] = data["renseignements_orNet"]  # TODO proportionnel à la surface ?
+
     # Redevance départementale :
-    "Tarifs",  # [col. 7]
-    "Montant net",  # [col. 8] (col. 6 x col. 7)
+    matrice["Tarifs (RDM)"] = data["tarifs_rdm"]
+    matrice["Montant net (RDM)"] = data["redevance_departementale_des_mines_aurifere_kg"]
+
     # Redevance communale :
-    "Tarifs",  # [col. 9]
-    "Montant net redevance des mines",  # [col. 10] (col. 6 x col. 9)
-    # ---
-    "Total redevance des mines",  # [col. 11] (col.8 + col. 10)
+    matrice["Tarifs (RCM)"] = data["tarifs_rcm"]
+    matrice["Montant net redevance des mines (RCM)"] = data["redevance_communale_des_mines_aurifere_kg"]
+     
+    matrice["Total redevance des mines"] = matrice["Montant net (RDM)"] + matrice["Montant net redevance des mines (RCM)"]
+
     # Taxe minière sur l'or de Guyane :
-    "PME",  # [col. 12] Tarifs par kg extrait pour les...
-    "Autres entreprises",  # [col. 13] Tarifs par kg extrait pour les...
-    # > Camino : titres.amodiataires_categorie ou titres.titulaires_categorie
-    "Montant des investissements déduits",
-    # [col. 14]
-    # > Camino : somme des 4 rapports trimestriels année N-1 (activités.renseignements_environnement)
-    # > pour activités.annee en N-1 et activités.period en 'année'
-    # > = sur l'interface web, activités, "Dépenses relatives à la protection de l’environnement (euros)"
-    "Montant net de taxe minière sur l'or de Guyane", # [col. 15] (col.6 x col.12)-col.14 ou (col.6 x col.13)-col.14
-    # ---
-    "Frais de gestion de la fiscalité directe locale",  # [col. 16] (col.11 + col.15) x 8%
-    "Service de la Direction générale des Finances publiques en charge du recouvrement",
-    # [col. 17]
-    # > Camino : titre.administrations_noms ?
-    "Numéro de l’article du rôle",  # [col. 18]
-    "Observations, précisions"
-    # [col. 19]
-    # > Camino : activités.complement_texte ?
-]  # une ligne par titre
+    matrice["PME"] = data["taxe_tarif_pme"]
+    matrice["Autres entreprises"] = data["taxe_tarif_autres"]
+    matrice["Montant des investissements déduits"] = data["investissement"]
+
+    # TODO add check
+    # pandas.testing.assert_series_equal(data["taxe_guyane_brute"], (
+    #     (
+    #         (matrice["Quantités (kg)"] * matrice["PME"])
+    #             - numpy.where(data["categorie_entreprise"] == "PME", matrice["Montant des investissements déduits"], 0)
+    #         ) + (
+    #             (matrice["Quantités (kg)"] * matrice["Autres entreprises"])
+    #             - numpy.where(data["categorie_entreprise"] != "PME", matrice["Montant des investissements déduits"], 0)
+    #             )
+    #         ).astype('float32')
+    #     )
+    matrice["Montant net de taxe minière sur l'or de Guyane"] = data["taxe_guyane_brute"]
+
+    matrice["Frais de gestion de la fiscalité directe locale"] = (
+        matrice["Total redevance des mines"] + matrice["Montant net de taxe minière sur l'or de Guyane"]
+    ) * 0.08
+
+    matrice["Service de la Direction générale des Finances publiques en charge du recouvrement"] = data["drfip"]
+    matrice["Numéro de l’article du rôle"] = data["titre_id"]
+    matrice["Observations, précisions"] = data["observation"]
+
+    matrice.to_csv(f'matrice_drfip_guyane_production_{annee_production}_{timestamp}.csv', index=False)
 
 
 # Template ÉTAT ANNEXE À LA MATRICE 1122
