353fa99824537d1f74aa23b3cc3ca34f45a55fa5
sandcha <sandcha@users.noreply.github.com>
2021-12-13 20:32:53 +0100

    Déclare les SIP de Guyane

---
+++ simulations/sip.py
@@ -0,0 +1,25 @@
+# SIP = services des impôts des particuliers
+
+from typing import List
+
+
+sip_guyane_cayenne_nom = "SIP de Cayenne"
+sip_guyane_cayenne: List[str] = [
+    "Montsinery-Tonnegrande", "Ouanary", "Camopi", "Cayenne",
+    "Régina", "Remire-Montjoly", "Roura", 
+    "Saint-Georges", "Matoury"
+    ]
+
+sip_guyane_kourou_nom = "SIP de Kourou"
+sip_guyane_kourou: List[str] = ["Kourou", "Macouria", "Iarcoubo", "Sinnamary", "Saint-Elie"]
+
+sip_guyane_st_laurent_du_maroni_nom = "SIP de Saint-Laurent du Maroni"
+sip_guyane_st_laurent_du_maroni: List[str] = [
+    "Papaichton", "Apatou", "Awala-Yalimapo",
+    "Grand-Santi", "Mana", "Saül", "Maripasoula",
+    "Saint-Laurent du Maroni"
+    ]
+
+def get_sip_data(data, column_name, sip):
+    filter = data[column_name].isin(sip)
+    return data[filter]
